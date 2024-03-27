fcc925070ae9aaa2f3558c5be7bda97b4440b236
Vitoria5555 <160559132+Vitoria5555@users.noreply.github.com>
2024-03-27 10:17:14 -0300

    Add files via upload

---
+++ aula dia 27 py resultado.py
@@ -0,0 +1,192 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ===========
+soma (6,5)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    soma (6,5)
+  File "C:/Users/202101297814/Exercicios/aula dia 27.py", line 2, in soma
+    return a + b5
+NameError: name 'b5' is not defined. Did you mean: 'b'?
+
+=========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ===========
+soma (6,5)
+11
+
+=========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ===========
+quadrado (4)
+16
+hipoternusa (3,4)
+5.0
+
+=========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ===========
+simples ( cor)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    simples ( cor)
+NameError: name 'cor' is not defined. Did you mean: 'chr'?
+simples (azul)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    simples (azul)
+NameError: name 'azul' is not defined
+simples ( 'azul')
+' escolheu certo'
+
+=========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ===========
+medio ( 'azul')
+'escolheu certo'
+return
+SyntaxError: 'return' outside function
+return ' amarelo'
+SyntaxError: 'return' outside function
+medio ( ' azul')
+'tente outra cor'
+medio ( 'amarelo')
+'tente outra cor'
+completo ( 'azul')
+' escolheu certo'
+completo ('marrom')
+' não tem salvação'
+else
+SyntaxError: invalid syntax
+
+========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ==========
+1
+5
+[10, 2, 3, 4, 5]
+
+========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ==========
+1
+5
+[10, 2, 3, 4, 5]
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ==========
+1
+5
+[10, 2, 3, 4, 5]
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+1
+45
+78
+a
+[3, 5]
+m
+i
+n
+h
+a
+ 
+s
+t
+r
+i
+n
+g
+
+========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ==========
+1
+5
+[10, 2, 3, 4, 5]
+=== começando do while===
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+=== começando o for com range (10) ===
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+1
+45
+78
+a
+[3, 5]
+m
+i
+n
+h
+a
+ 
+s
+t
+r
+i
+n
+g
+
+========== RESTART: C:/Users/202101297814/Exercicios/aula dia 27.py ==========
+1
+5
+[10, 2, 3, 4, 5]
+=== começando do while===
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+=== começando o for com range (10) ===
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+1
+45
+78
+a
+[3, 5]
+m i n h a   s t r i n g 
